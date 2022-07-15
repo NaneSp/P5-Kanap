4b0de1740567bc9c5b246bfdadf033973779d83d
Spinon <stephaniepinon160609@gmail.com>
2022-07-15 12:17:08 +0200

    envoi

---
+++ front/js/script.js
@@ -0,0 +1,60 @@
+
+//récupération des informations de l'api :
+
+fetch("http://localhost:3000/api/products")
+//promesse faite à l'api si succès:
+
+    .then(function(response){
+        //retourne la réponse au format json
+        return response.json();
+    })
+
+    .then (function(apiArticles){
+
+        console.log(apiArticles);
+        //retourne en réponse un tableau contenant plusieurs articles
+
+
+        //création d'une boucle for...of pour répeter les tâches ->travail sur un array 
+        for (let article of apiArticles){
+
+        console.log(article);
+        //retourne en réponse plusieurs tableaux détaillant chaque articles
+
+
+        const itemsLink = 
+        document.createElement("a");// créé un élement lien
+        document.querySelector(".items");//va chercher l'élément dans le dom
+        items.appendChild(itemsLink);//rajoute à l'élément du dom un enfant
+        itemsLink.href = `../html/product.html?id=${article._id}`;// 
+        
+        const itemsArticle = // fait apparaitre carte bleue foncée ds navigateur
+        document.createElement("article");
+        itemsLink.appendChild(itemsArticle);
+        
+
+        const itemsImg = // fait apparaitre contours blancs dans la carte itemsArticle
+        document.createElement("img");
+        itemsArticle.appendChild(itemsImg);
+        itemsImg.src = article.imageUrl;//ajoute la source de l'img depuis l'api
+        itemsImg.alt = article.altTxt;//ajoute le alt de l'img depuis l'api
+
+        const itemsH3 =
+        document.createElement("h3");
+        itemsArticle.appendChild(itemsH3);
+        itemsH3.textContent = article.name;//ajoute les titres 
+
+        const itemsParagraph =
+        document.createElement("p");
+        itemsArticle.appendChild(itemsParagraph);
+        itemsParagraph.textContent = article.description;//ajoute les textes descriptifs
+    }
+})
+
+
+    .catch (function (error){
+    //si erreur retourne le mot Erreur 
+    alert("Erreur"); 
+    })
+
+    
