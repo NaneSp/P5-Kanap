3a31d15574ce79b70884820694b88917d40569da
Spinon <stephaniepinon160609@gmail.com>
2022-07-27 09:53:10 +0200

    test LS/ revoir couleur forEach

--- front/js/product.js
+++ front/js/product.js
@@ -1,54 +1,95 @@
+//récupération des paramètres URL pour l'id
 let params = window.location.href; // renvoi le href (url) de la page en cours
-let url = new URL(params);
-let idArticle = url.searchParams.get("id");
-console.log(idArticle); //renvoi bien l'id de l'article selectionné en page d'accueil
+let url = new URL(params); //crée un nvel objet
+let idArticle = url.searchParams.get("id"); //id renseigné en paramètre
+//console.log(idArticle); //renvoi bien l'id de l'article selectionné en page d'accueil
 
-
-
+//récupération de l'article par son id
 fetch("http://localhost:3000/api/products/" + idArticle)
-
-.then(function (response) {
+    .then(function (response) {
     //retourne la réponse au format json
     return response.json();
-})
+    })
+
+    .then(function (idArticle) {
+   // console.table(idArticle);
+//retourne en réponse un tableau de l'article en détail
 
 
-.then(function (idArticle) {
-    console.table(idArticle);
-    //retourne en réponse un tableau de l'article en détail
-
-    article = idArticle;
-    
-    const itemImg = document.createElement("img");//création de la balise img
-    document.querySelector(".item__img").appendChild(itemImg);//rajoute un enfant à l'élément (code raccourci)
-    console.log(itemImg);//renvoi bien la balise img dans la console
-    itemImg.src = article.imageUrl;
-    itemImg.alt = article.altTxt;
+    const itemImg = document.createElement("img"); //création de la balise img
+    document.querySelector(".item__img").appendChild(itemImg); //rajoute un enfant à l'élément (code raccourci)
+    //console.log(itemImg); //renvoi bien la balise img dans la console
+    itemImg.src = idArticle.imageUrl;
+    itemImg.alt = idArticle.altTxt;
 
     const itemName = document.querySelector("#title");
-    console.log(itemName); // renvoi bien le title h1  du dom ds la console
-    itemName.textContent = article.name;
+    //console.log(itemName); // renvoi bien le title h1  du dom ds la console
+    itemName.textContent = idArticle.name;
 
     const itemPrice = document.querySelector("#price");
-    console.log(itemPrice); //renvoi bien le price span  du dom ds la concole
-    itemPrice.textContent = article.price;
+    //console.log(itemPrice); //renvoi bien le price span  du dom ds la concole
+    itemPrice.textContent = idArticle.price;
 
     const itemDescription = document.querySelector("#description");
-    console.log(itemDescription); // renvoi bien la description p  du dom ds la console
-    itemDescription.textContent = article.description;
+    //console.log(itemDescription); // renvoi bien la description p  du dom ds la console
+    itemDescription.textContent = idArticle.description;
 
-    //création d'une boucle for...of pour répeter les tâches de choix de couleurs
-    for( let colors of article.colors){ 
-    console.log(colors);// renvoi bien les couleurs dispo ds la console
-    const itemColor = document.createElement("option");//création d'une nvelle balise option pr choix des couleurs
-    document.querySelector("#colors").appendChild(itemColor);//ajoute un enfant
+//création d'une boucle for...of pour répeter les tâches de choix de couleurs
+
+    for (let colors of idArticle.colors) {
+      //console.log(colors); // renvoi bien les couleurs dispo ds la console
+    const itemColor = document.createElement("option"); //création d'une nvelle balise option pr choix des couleurs
+    document.querySelector("#colors").appendChild(itemColor);
     itemColor.value = colors;
     itemColor.textContent = colors;
     }
+    })
 
-});
+    .catch(function (error) {
+//si erreur retourne message d'erreur
+    alert("Erreur!!!");
+    });
 
-/* .catch (function(error){
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
-        alert ("Erreur!!!")
-    })*/
+
+
+
+    const colorChoice = document.querySelector("#colors");//va chercher l'id colors ds le dom
+    const quantityChoice = document.querySelector ("#quantity");// va chercher l'id quantity ds le dom
+    const addToCartBtn = document.querySelector("#addToCart"); // va chercher l'id addToCart ds le dom
+    
+
+addToCartBtn.addEventListener ("click", ()=>{
+
+//création de l'objet à insérer ds le LocalStorage
+    const articleSelected ={
+            idSelected : idArticle,
+            quantitySelected : quantityChoice.value,
+            colorSelected : colorChoice.value
+            }
+
+
+
+// Déclaration de la variable "articleLocalStorage" dans laquelle on met clé/valeur qui sont ds le LS
+    let articleLocalStorage = JSON.parse(localStorage.getItem("LSArticle"));// json.parse convertit les données au format JSON qui sont ds le LS en objet JS
+        
+
+
+
+
+// si il n'y a pas d'article enregistré ds le LS
+    if (articleLocalStorage == null){
+        articleLocalStorage =[]; //création d'un array vide
+        articleLocalStorage.push(articleSelected); // je push la variable objet ds le tableau
+        localStorage.setItem("LSArticle", JSON.stringify("articleLocalStorage"));//j'envoie les valeurs de la variable ds le ls 
+        alert ("Votre article a bien été ajouté au panier")
+        console.log(articleLocalStorage);//vérif que le tableau s'affiche bien ds la console
+        }
+        //si il y a 1 ou des articles déjà présents dans le LS
+        else{
+
+
+        }
+
+    })
--- front/js/script.js
+++ front/js/script.js
@@ -12,35 +12,38 @@ fetch("http://localhost:3000/api/products")
     console.log(apiArticles);
     //retourne en réponse un tableau contenant plusieurs articles
 
-    //création d'une boucle for...of (parcourt les valeurs des propriétés)pour répeter les tâches ->travail sur un array
-    for (let article of apiArticles) {
-    console.log(article);
-      //retourne en réponse plusieurs tableaux détaillant chaque articles
 
-    const itemsLink = document.createElement("a"); // créé un élement lien
-    document.querySelector(".items"); //va chercher l'élément dans le dom
-    items.appendChild(itemsLink); //rajoute à l'élément du dom un enfant
-    itemsLink.href = `../html/product.html?id=${article._id}`; //
+    apiArticles.forEach(article => {
 
-    const itemsArticle = document.createElement("article"); // fait apparaitre carte bleue foncée ds navigateur
-    itemsLink.appendChild(itemsArticle);
+      console.log(article);
 
-    const itemsImg = document.createElement("img"); // fait apparaitre contours blancs dans la carte itemsArticle
-    itemsArticle.appendChild(itemsImg);
-    itemsImg.src = article.imageUrl; //ajoute la source de l'img depuis l'api
-    itemsImg.alt = article.altTxt; //ajoute le alt de l'img depuis l'api
+      const itemsLink = document.createElement("a"); // créé un élement lien
+      document.querySelector(".items"); //va chercher l'élément dans le dom
+      items.appendChild(itemsLink); //rajoute à l'élément du dom un enfant
+      itemsLink.href = `../html/product.html?id=${article._id}`; //
+  
+      const itemsArticle = document.createElement("article"); // fait apparaitre carte bleue foncée ds navigateur
+      itemsLink.appendChild(itemsArticle);
+  
+      const itemsImg = document.createElement("img"); // fait apparaitre contours blancs dans la carte itemsArticle
+      itemsArticle.appendChild(itemsImg);
+      itemsImg.src = article.imageUrl; //ajoute la source de l'img depuis l'api
+      itemsImg.alt = article.altTxt; //ajoute le alt de l'img depuis l'api
+  
+      const itemsH3 = document.createElement("h3");
+      itemsArticle.appendChild(itemsH3);
+      itemsH3.textContent = article.name; //ajoute les titres
+  
+      const itemsParagraph = document.createElement("p");
+      itemsArticle.appendChild(itemsParagraph);
+      itemsParagraph.textContent = article.description; //ajoute les textes descriptifs
+      
+    
+    });
 
-    const itemsH3 = document.createElement("h3");
-    itemsArticle.appendChild(itemsH3);
-    itemsH3.textContent = article.name; //ajoute les titres
-
-    const itemsParagraph = document.createElement("p");
-    itemsArticle.appendChild(itemsParagraph);
-    itemsParagraph.textContent = article.description; //ajoute les textes descriptifs
-    }
 })
 
 .catch(function (error) {
-    //si erreur retourne le mot Erreur
+    //si erreur retourne un message d'erreur
     alert("Erreur! Avez-vous bien lancé le serveur local (port 3000)");
 });
